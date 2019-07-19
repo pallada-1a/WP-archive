a84a134ea11d56a29de7c681a0565432a9d52dcf
pallada-1a <44030193+pallada-1a@users.noreply.github.com>
2019-07-19 11:02:18 +0200

    Create sn.php

---
+++ sn.php
@@ -0,0 +1,34 @@
+/*remove*/
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+remove_action('wp_head', 'rsd_link');
+
+
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+remove_action('wp_head','adjacent_posts_rel_link_wp_head', 10, 0 );
+remove_action('wp_head', 'wp_resource_hints', 2 );
+remove_action('wp_head', 'rest_output_link_wp_head', 10, 0 );
+remove_action('wp_head','wp_oembed_add_discovery_links',10);
+/*---------------------------*/
+function remove_core_updates(){
+global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
+/*---------------------------*/
+//Disable gutenberg editor style in Front Meta
+add_action( 'wp_print_styles', 'wps_deregister_styles', 100 );
+function wps_deregister_styles() {
+    wp_dequeue_style( 'wp-block-library' );
+}
